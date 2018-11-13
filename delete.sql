8afc895e9c7ef756539139bb1ae0503b3330a4f3
Fahad2314 <43133826+Fahad2314@users.noreply.github.com>
2018-11-13 21:52:43 +0000

    Update delete.sql

--- delete.sql
+++ delete.sql
@@ -1,11 +1,18 @@
 
 --DELETE STATEMENT FOR THE MOST CONTROVERSIAL
-DELETE employee 
+
+DELETE
 FROM employee 
-JOIN (SELECT comment_creator_id  
-    FROM comments 
-    GROUP BY status_id 
-    ORDER BY COUNT(comment_id) DESC
-    LIMIT 1) AS T
-ON employee_id = comment_creator_id
-AND (employee_id= comment_creator_id);
+WHERE employee.emp_email = (SELECT asd FROM 
+    (SELECT emp_email AS asd
+                                FROM employee e 
+                                INNER JOIN 
+                                                (SELECT * FROM statuses s
+                                                INNER JOIN 
+                                                            (SELECT COUNT(comment_id) AS 'Comment Count', status_id AS s_id,comment_text
+                                                                            FROM comments 
+                                                                            GROUP BY s_id 
+                                                                            ORDER BY COUNT(comment_id) DESC
+                                                                            LIMIT 1) AS T
+                                                ON s.status_id = T.s_id) AS T2
+                                ON e.employee_id = T2.creator_id) AS T5 );
